76cb504a62fc533bec6e2ec09e52d3de4bec5411
Farzeen Zehra <farzeenzehra.fz@gmail.com>
2022-10-10 00:36:36 +0500

    body

--- src/routes/doctors.js
+++ src/routes/doctors.js
@@ -42,16 +42,17 @@ route.get('/all', async (req, res)=>{
 })
 route.post('/login',
     async (req, res) => {
+        const {body} = req.body;
         try {
             //find a Doctor with the email
-            let doctor = await Doctor.findOne({ email: req.body.email });
+            let doctor = await Doctor.findOne({ email: body.email });
             console.log(doctor)
             console.log(req.body)
             if (!doctor) {
                 return res.status(400).json({ message: 'Please Signup first' })
             }
 
-            const passwordCompare = await bcrypt.compare(req.body.password, doctor.password)
+            const passwordCompare = await bcrypt.compare(body.password, doctor.password)
             if (!passwordCompare) {
                 res.status(400).json({ type: "error", message: 'Please enter valid credentials' })
             }
