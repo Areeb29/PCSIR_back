c5d598b1c484e1d0286dd3dc4e0bc5a060674437
Farzeen Zehra <farzeenzehra.fz@gmail.com>
2022-10-09 23:10:26 +0500

    middleware

--- index.js
+++ index.js
@@ -16,7 +16,11 @@ app.use(express.json())
 app.use(fileupload());
 app.use(express.static(__dirname));
 app.use(express.static(__dirname+"\\files"));
-
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 //Available Routes
 app.use('//', (req, res)=>res.send("Hello World!"))
 app.use('/image',require('./src/routes/image'));
